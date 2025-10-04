55c23be614eebcdf129b7a2ae8b1d289293c35b0
shalina maniyar <shalinamaniyar@gmail.com>
2025-10-04 11:16:02 +0530

    version13

--- account.py
+++ account.py
@@ -4,3 +4,10 @@ print("Initial amount:",initial)
 print("Deposit amount:",deposit)
 balance = initial + deposit
 print(f"Updated balance after deposit: ₹{balance:.2f}")
+withdraw_amount = float(input("Enter the amount to withdraw: "))
+
+if withdraw_amount <= balance:
+    balance -= withdraw_amount
+    print(f"Withdrawal successful! New balance: ₹{balance:.2f}")
+else:
+    print("Insufficient balance!")
